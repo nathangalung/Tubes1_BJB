afa1229be9e1e1aefb648bc413f16eb80c99ccf8
nathangalung <18222130@std.stei.itb.ac.id>
2025-03-04 23:10:51 +0700

    feat: base structure

---
+++ src/alternative-bots/nelson/nelson.cs
@@ -0,0 +1,55 @@
+using System.Drawing;
+using Robocode.TankRoyale.BotApi;
+using Robocode.TankRoyale.BotApi.Events;
+
+public class nelson : Bot
+{
+    // The main method starts our bot
+    static void Main(string[] args)
+    {
+        new nelson().Start();
+    }
+
+    // Constructor, which loads the bot config file
+    nelson() : base(BotInfo.FromFile("nelson.json")) { }
+
+    // Called when a new round is started -> initialize and do some movement
+    public override void Run()
+    {
+
+        BodyColor = Color.FromArgb(0xFF, 0x8C, 0x00);   // Dark Orange
+        TurretColor = Color.FromArgb(0xFF, 0xA5, 0x00); // Orange
+        RadarColor = Color.FromArgb(0xFF, 0xD7, 0x00);  // Gold
+        BulletColor = Color.FromArgb(0xFF, 0x45, 0x00); // Orange-Red
+        ScanColor = Color.FromArgb(0xFF, 0xFF, 0x00);   // Bright Yellow 
+        TracksColor = Color.FromArgb(0x99, 0x33, 0x00); // Dark   
+        Brownish-Orange
+        GunColor = Color.FromArgb(0xCC, 0x55, 0x00);    // Medium Orange
+      
+        // Repeat while the bot is running
+        while (IsRunning)
+        {
+            Forward(100);
+            TurnGunRight(360);
+            Back(100);
+            TurnGunRight(360);
+        }
+    }
+
+    // We saw another bot -> fire!
+    public override void OnScannedBot(ScannedBotEvent evt)
+    {
+        Fire(1);
+    }
+
+    // We were hit by a bullet -> turn perpendicular to the bullet
+    public override void OnHitByBullet(HitByBulletEvent evt)
+    {
+        // Calculate the bearing to the direction of the bullet
+        var bearing = CalcBearing(evt.Bullet.Direction);
+
+        // Turn 90 degrees to the bullet direction based on the bearing
+        TurnLeft(90 - bearing);
+    }
+}
+
